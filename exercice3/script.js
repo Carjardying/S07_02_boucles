763d99d34c389a45ba8e316ea7ad90b17c47b8e7
Carjardying <carolinejardy.pro@gmail.com>
2025-03-05 15:21:51 +0100

    exercice 3 part 1 + modif d'un truc sur ex1

--- exercice3/script.js
+++ exercice3/script.js
@@ -1,7 +1,11 @@
-/* Nous souhaitons créer un décompte.
+// Nous souhaitons créer un décompte.
 
-1- Demande à l'utilisateur (à l'aide de prompt) un nombre entre 0 et 10.
-2- Affiche ensuite tous les nombres sous le nombre indiqué par l'utilisateur (s'il rentre 4, on affichera 4, 3, 2, 1, 0).
-Attention, si l'utilisateur rentre un chiffre erroné il faudra afficher message d'erreur.
+// 1- Demande à l'utilisateur (à l'aide de prompt) un nombre entre 0 et 10.
+let askNumber = Number(prompt("Donne moi un nombre entre 0 et 10")); //number() fait la même chose qu'un parseInt.
+// 2- Affiche ensuite tous les nombres sous le nombre indiqué par l'utilisateur (s'il rentre 4, on affichera 4, 3, 2, 1, 0).
+for(let i= askNumber ; i>=0 ; i--){
+    console.log(i)
+}
+// Attention, si l'utilisateur rentre un chiffre erroné il faudra afficher message d'erreur.
 
- */
+//
